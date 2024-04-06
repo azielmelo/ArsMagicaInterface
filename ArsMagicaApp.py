ee72d43994f82bf6f1d41d4d2903db359f396a3d
azielmelo <azielmeru@gmail.com>
2024-04-06 00:32:26 -0300

    Adicionada habilidades conectadas ao xp, sem banco de dados, por inikspoinibilidade dce internet

--- ArsMagicaApp.py
+++ ArsMagicaApp.py
@@ -1,54 +1,42 @@
-import ficha
 import os
-import random
+from kivy.app import App
+from kivy.lang import Builder
+# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+# from kivy.properties import ObjectProperty
+# from kivy.uix.textinput import TextInput
+# from kivy.uix.label import Label
+# from kivy.uix.button import Button
+# from kivy.uix.dropdown import DropDown
+# from kivy.uix.image import AsyncImage
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
-import mysql.connector
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.dropdown import DropDown
-from kivy.uix.image import AsyncImage
-
-
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database = "banco"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SELECT * FROM usuario")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
 
 class FichaScreen(Screen):
-    def calculaCustoCarac(self):
-        Int = self.retornaInteiro(self.ids.Inteligencia.text)
-        Per = self.retornaInteiro(self.ids.Percepção.text)
-        For = self.retornaInteiro(self.ids.Força.text)
-        Vig = self.retornaInteiro(self.ids.Vigor.text)
-        Pre = self.retornaInteiro(self.ids.Presença.text)
-        Com = self.retornaInteiro(self.ids.Comunicação.text)
-        Des = self.retornaInteiro(self.ids.Destreza.text)
-        Rap = self.retornaInteiro(self.ids.Rapidez.text)
-        custoCarac = self.calculaProgAri(Int) + self.calculaProgAri(Per) + self.calculaProgAri(For) + self.calculaProgAri(Vig) + self.calculaProgAri(Pre) + self.calculaProgAri(Com) + self.calculaProgAri(Des) + self.calculaProgAri(Rap)
-        ptDispon = 7 - custoCarac
+
+    def calcula_custo_carac(self):
+        Int = self.retorna_inteiro(self.ids.Inteligencia.text)
+        Per = self.retorna_inteiro(self.ids.Percepção.text)
+        For = self.retorna_inteiro(self.ids.Força.text)
+        Vig = self.retorna_inteiro(self.ids.Vigor.text)
+        Pre = self.retorna_inteiro(self.ids.Presença.text)
+        Com = self.retorna_inteiro(self.ids.Comunicação.text)
+        Des = self.retorna_inteiro(self.ids.Destreza.text)
+        Rap = self.retorna_inteiro(self.ids.Rapidez.text)
+        custo_carac = self.calculaProgAri(Int) + self.calculaProgAri(Per) + self.calculaProgAri(For) + self.calculaProgAri(Vig) + self.calculaProgAri(Pre) + self.calculaProgAri(Com) + self.calculaProgAri(Des) + self.calculaProgAri(Rap)
+        ptDispon = 7 - custo_carac
         self.ids.ptCarac.text = str(ptDispon)
 
-    def retornaInteiro(self, strObj): ##Pega o valor de uma label, string, e retorna um inteiro
+    def calcula_custo_hab(self):
+        xp = (self.retorna_inteiro(self.ids.idade.text)-5)*15
+        hab1 = self.retorna_inteiro(self.ids.armas_de_uma_mao.text)
+        hab2 = self.retorna_inteiro(self.ids.armas_de_duas_maos.text)
+        custoHab = self.calculaProgAri(hab1)*5 + self.calculaProgAri(hab2)*5
+        xp = xp - custoHab
+        self.ids.xp.text = str(xp)
+
+    def retorna_inteiro(self, strObj): ##Pega o valor de uma label, string, e retorna um inteiro
         if strObj != '':
             return int(strObj)
         return 0
@@ -58,19 +46,10 @@ class FichaScreen(Screen):
             return n*(n+1)/2
         return n*(-n+1)/2
 
-    def salvarFicha(self):
-        sql = ("insert into fichas (inteligência)"
-               " value ")
-        val = self.retornaInteiro(self.ids.Inteligencia.text)
-        sql = sql + '(' + str(val) + ')'
-        print(sql)
-        mycursor.execute(sql)
-        mydb.commit()
-
     def __init__(self, **kwargs):
         super(FichaScreen, self).__init__(**kwargs)
         self.ids.tipodropdown.dismiss() ##fecha o tipo drop down, sem essa função o tipo dropdown aaparece aberto logo no início da aplicação
-        self.calculaCustoCarac() ##preenche o n° de pontos disponíveis para caracteristicas, vai ser importante quando fichas buscar a ficha específica no banco de dados.
+        self.calcula_custo_carac() ##preenche o n° de pontos disponíveis para caracteristicas, vai ser importante quando fichas buscar a ficha específica no banco de dados.
 
 class ArsmagicaApp(App):
     def build(self):
@@ -90,5 +69,3 @@ sm.current = "ficha"
 
 if __name__ == '__main__':
     ArsmagicaApp().run()
-
-print(ficha.MagoIgnácio.FOR)
