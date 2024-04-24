b64149f0ec95a8c0f71ceea33f567cc544c8ff7b
azielmelo <azielmeru@gmail.com>
2024-04-23 21:19:09 -0300

    adicionado código para dropdown

--- ArsMagicaApp.py
+++ ArsMagicaApp.py
@@ -7,7 +7,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 # from kivy.uix.textinput import TextInput
 # from kivy.uix.label import Label
 # from kivy.uix.button import Button
-# from kivy.uix.dropdown import DropDown
+from kivy.uix.dropdown import DropDown
 # from kivy.uix.image import AsyncImage
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
@@ -15,26 +15,26 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 class FichaScreen(Screen):
 
-    def calcula_custo_carac(self):
-        Int = self.retorna_inteiro(self.ids.Inteligencia.text)
-        Per = self.retorna_inteiro(self.ids.Percepção.text)
-        For = self.retorna_inteiro(self.ids.Força.text)
-        Vig = self.retorna_inteiro(self.ids.Vigor.text)
-        Pre = self.retorna_inteiro(self.ids.Presença.text)
-        Com = self.retorna_inteiro(self.ids.Comunicação.text)
-        Des = self.retorna_inteiro(self.ids.Destreza.text)
-        Rap = self.retorna_inteiro(self.ids.Rapidez.text)
-        custo_carac = self.calculaProgAri(Int) + self.calculaProgAri(Per) + self.calculaProgAri(For) + self.calculaProgAri(Vig) + self.calculaProgAri(Pre) + self.calculaProgAri(Com) + self.calculaProgAri(Des) + self.calculaProgAri(Rap)
-        ptDispon = 7 - custo_carac
-        self.ids.ptCarac.text = str(ptDispon)
-
-    def calcula_custo_hab(self):
-        xp = (self.retorna_inteiro(self.ids.idade.text)-5)*15
-        hab1 = self.retorna_inteiro(self.ids.armas_de_uma_mao.text)
-        hab2 = self.retorna_inteiro(self.ids.armas_de_duas_maos.text)
-        custoHab = self.calculaProgAri(hab1)*5 + self.calculaProgAri(hab2)*5
-        xp = xp - custoHab
-        self.ids.xp.text = str(xp)
+    # def calcula_custo_carac(self):
+    #     # Int = self.retorna_inteiro(self.ids.Inteligencia.text)
+    #     # Per = self.retorna_inteiro(self.ids.Percepção.text)
+    #     # For = self.retorna_inteiro(self.ids.Força.text)
+    #     # Vig = self.retorna_inteiro(self.ids.Vigor.text)
+    #     # Pre = self.retorna_inteiro(self.ids.Presença.text)
+    #     # Com = self.retorna_inteiro(self.ids.Comunicação.text)
+    #     # Des = self.retorna_inteiro(self.ids.Destreza.text)
+    #     # Rap = self.retorna_inteiro(self.ids.Rapidez.text)
+    #     # custo_carac = self.calculaProgAri(Int) + self.calculaProgAri(Per) + self.calculaProgAri(For) + self.calculaProgAri(Vig) + self.calculaProgAri(Pre) + self.calculaProgAri(Com) + self.calculaProgAri(Des) + self.calculaProgAri(Rap)
+    #     # ptDispon = 7 - custo_carac
+    #     # self.ids.ptCarac.text = str(ptDispon)
+    #
+    # def calcula_custo_hab(self):
+    #     # xp = (self.retorna_inteiro(self.ids.idade.text)-5)*15
+    #     # hab1 = self.retorna_inteiro(self.ids.armas_de_uma_mao.text)
+    #     # hab2 = self.retorna_inteiro(self.ids.armas_de_duas_maos.text)
+    #     # custoHab = self.calculaProgAri(hab1)*5 + self.calculaProgAri(hab2)*5
+    #     # xp = xp - custoHab
+    #     # self.ids.xp.text = str(xp)
 
     def retorna_inteiro(self, strObj): # Pega o valor de uma label, string, e retorna um inteiro
         if strObj != '':
@@ -48,12 +48,13 @@ class FichaScreen(Screen):
 
     def __init__(self, **kwargs):
         super(FichaScreen, self).__init__(**kwargs)
-        self.ids.tipodropdown.dismiss() # fecha o tipo drop down, sem essa função o tipo dropdown aaparece aberto logo no início da aplicação
-        self.calcula_custo_carac() # preenche o n° de pontos disponíveis para caracteristicas, vai ser importante quando fichas buscar a ficha específica no banco de dados.
+        #tipo_drop_down = TipoDropDown()
+        #self.ids.tipo_botão.bind(on_release = tipo_drop_down.open)
+        # self.ids.tipodropdown.dismiss() # fecha o tipo drop down, sem essa função o tipo dropdown aaparece aberto logo no início da aplicação
+        # self.calcula_custo_carac() # preenche o n° de pontos disponíveis para caracteristicas, vai ser importante quando fichas buscar a ficha específica no banco de dados.
 
-class ArsmagicaApp(App):
-    def build(self):
-        return sm
+class TipoDropDown(DropDown):
+    pass
 
 kv = Builder.load_file("ArsMagica.kv")
 sm = ScreenManager()
@@ -65,7 +66,9 @@ for Screen in screens:
 
 sm.current = "ficha"
 
-
+class ArsmagicaApp(App):
+    def build(self):
+        return sm
 
 if __name__ == '__main__':
     ArsmagicaApp().run()
